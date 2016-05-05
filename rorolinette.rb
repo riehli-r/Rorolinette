1f17ca9537fed88091d3841df0aefcec7b74813b
riehli-r <riehli_r@etna-alternance.net>
2016-05-05 17:05:30 +0200

    Colors

--- rorolinette.rb
+++ rorolinette.rb
@@ -6,12 +6,20 @@
 #
 ##########################
 
+##------------COLORS-------------
+
+@blue = "\033[1;34m"
+@red = "\033[1;31m"
+@default = "\033[0m"
+
+@error = 0
 
 def checkHeader(file)
   nbrLine = 0
   file.each_line do |line| 
     if nbrLine <= 6 && (line.split("")[0] != '/' || line.split("")[0] != '*')
-      puts "line #{nbrLine} : Header incorrect"
+      puts "----line #{@blue}#{nbrLine}#{@default} : Header incorrect"
+      @error += 1
     end
     nbrLine += 1
   end
